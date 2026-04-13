16d120e2ae228be2c797fac37c22f8f356aec014
Jaswant B <90975111+JaswantB@users.noreply.github.com>
2026-04-13 07:59:15 +0530

    Added Pooling

---
+++ Assets/Scripts/Pooling.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+public class Pooling : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private int poolSize = 20;
+    public Transform enemyParent;
+    private List<GameObject> enemies = new List<GameObject>();
+
+    void Awake()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, transform, enemyParent);
+            enemy.SetActive(false);
+            enemies.Add(enemy);
+        }
+    }
+
+    public GameObject GetEnemy()
+    {
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            if (!enemies[i].activeInHierarchy == false)
+            {
+                enemies[i].SetActive(true);
+                return enemies[i];
+            }
+        }
+        GameObject newEnemy=Instantiate(enemyPrefab,transform,enemyParent);
+        enemies.Add(newEnemy);
+        return newEnemy;
+    }
+    public void ReturnEnemy(GameObject enemy)
+    {
+        enemy.SetActive(false);
+    }
+
+}
